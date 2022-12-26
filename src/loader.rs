9cc017a85ff82513cf454622fe1b1d6976936051
Nishant Joshi <nishantjo.12@gmail.com>
2022-12-26 19:03:46 +0530

    fix(loader): fix board display config

--- src/loader.rs
+++ src/loader.rs
@@ -1,7 +1,7 @@
-use embedded_graphics::{prelude::*, pixelcolor::BinaryColor};
 
 #[cfg(feature = "sim")]
 pub mod disp {
+    use embedded_graphics::{prelude::*, pixelcolor::BinaryColor};
     use std::ops::{Deref, DerefMut};
 
     use embedded_graphics_simulator::{SimulatorDisplay, OutputSettingsBuilder, Window};
@@ -53,13 +53,36 @@ pub mod disp {
 #[cfg(feature = "board")]
 pub mod disp {
     use ssd1306::prelude::*;
-    pub fn get_display() -> Result<ssd1306::Ssd1306<ssd1306::I2CInterface<rppal::i2c::I2c>, DisplaySize128x64, ssd1306::mode::BufferedGraphicsMode<DisplaySize128x64>>, Box<dyn std::error::Error>> {
+    use std::{fmt, ops};
+
+    #[derive(Debug)]
+    struct HalError<T: fmt::Debug>(T);
+    impl<T: fmt::Debug> fmt::Display for HalError<T> {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            fmt::Debug::fmt(&self.0, f)
+        }
+    }
+    impl<T: fmt::Debug> std::error::Error for HalError<T> {}
+
+    pub struct Wrapper<T>(T);
+
+    impl<T> ops::Deref for Wrapper<T> {
+        type Target = T;
+        fn deref(&self) -> &Self::Target { &self.0 }
+    }
+
+    impl<T> ops::DerefMut for Wrapper<T> {
+        fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
+    }
+
+
+    pub fn get_display() -> Result<Wrapper<ssd1306::Ssd1306<I2CInterface<rppal::i2c::I2c>, DisplaySize128x64, ssd1306::mode::BufferedGraphicsMode<DisplaySize128x64>>>, Box<dyn std::error::Error>> {
 
         let i2c_rpi_interface = rppal::i2c::I2c::new()?;
         let interface = ssd1306::I2CDisplayInterface::new(i2c_rpi_interface);
         let mut display = ssd1306::Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate180).into_buffered_graphics_mode();
-        display.init()?;
-        Ok(display)
+        display.init().map_err(|err| HalError(err))?;
+        Ok(Wrapper(display))
     }
 }
 

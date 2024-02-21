cb581c11badba99d703c18ce3d0d5e0ae5eae39c
Nishant Joshi <nishantjo.12@gmail.com>
2024-02-21 20:32:06 +0530

    chore: add support to take stdin

--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,30 @@ fn main() {
 
     let style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
 
+
+    let mut buf = String::new();
+    std::io::stdin().read_line(&mut buf).unwrap();
+
     // create text object
-    let text = embedded_graphics::text::Text::new("Hello, World", start, style);
+    let text = embedded_graphics::text::Text::new(&buf, start, style);
 
     text.draw(&mut *display).unwrap();
 
     display.flush().unwrap();
     // let mut delta = Point::new(1, 0);
     loop {
-        // let start = Instant::now();
-        // im.translate_mut(delta.clone());
+    //     // let start = Instant::now();
+    //     // im.translate_mut(delta.clone());
 
-        // match (delta.x, im.bounding_box().top_left.x) {
-        //     (1, x) if x > 64 => delta = Point::new(-1, 0),
-        //     (-1, x) if x < 0 => delta = Point::new(1, 0),
-        //     (_, _) => {}
-        // }
-        // display.clear();
-        // im.draw(&mut *display).unwrap();
-        // display.flush().unwrap();
-        // let total = Instant::now() - start;
-        // println!("Time Taken: {}ms", total.as_millis());
+    //     // match (delta.x, im.bounding_box().top_left.x) {
+    //     //     (1, x) if x > 64 => delta = Point::new(-1, 0),
+    //     //     (-1, x) if x < 0 => delta = Point::new(1, 0),
+    //     //     (_, _) => {}
+    //     // }
+    //     // display.clear();
+    //     // im.draw(&mut *display).unwrap();
+    //     // display.flush().unwrap();
+    //     // let total = Instant::now() - start;
+    //     // println!("Time Taken: {}ms", total.as_millis());
     }
 }

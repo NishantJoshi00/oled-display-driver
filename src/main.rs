eaa3caf217a008abe4c757c8c7a554584688787d
Nishant Joshi <nishantjo.12@gmail.com>
2024-02-21 22:24:32 +0530

    chore: this thing

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,11 @@ use loader::disp::get_display;
 const CHAR_LIMIT: usize = 20;
 
 // If the text is longer than CHAR_LIMIT, wrap it, do a word wrap
-fn wrap_text(text: String) -> String {
+fn wrap_text(text: String, size: usize) -> String {
     let mut wrapped = String::new();
     let mut count = 0;
     text.split_whitespace().for_each(|word| {
-        if count + word.len() > CHAR_LIMIT {
+        if count + word.len() > (CHAR_LIMIT * 6) / size {
             wrapped.push('\n');
             count = 0;
         }
@@ -39,18 +39,18 @@ fn main() {
     // get environment variable FONT_SIZE
     let font_size = std::env::var("FONT_SIZE").ok();
 
-    let font = match font_size.as_ref().map(|s| s.as_str()) {
-        Some("8") => ascii::FONT_8X13,
-        Some("9") => ascii::FONT_9X15,
-        Some("10") => ascii::FONT_10X20,
-        Some("6") | _ => ascii::FONT_6X10,
+    let (font, size) = match font_size.as_ref().map(|s| s.as_str()) {
+        Some("8") => (ascii::FONT_8X13, 6),
+        Some("9") => (ascii::FONT_9X15, 9),
+        Some("10") => (ascii::FONT_10X20, 10),
+        Some("6") | _ => (ascii::FONT_6X10, 6),
     };
 
     let style = MonoTextStyle::new(&font, BinaryColor::On);
 
     let mut buf = String::new();
     std::io::stdin().read_line(&mut buf).unwrap();
-    let buf = wrap_text(buf);
+    let buf = wrap_text(buf, size);
 
     // create text object
     let text = embedded_graphics::text::Text::new(&buf, start, style);

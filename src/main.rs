40f7a793feef309f550648bca78c02e69f3dcf54
Nishant Joshi <nishantjo.12@gmail.com>
2024-02-21 17:08:57 +0530

    fun

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::time::Instant;
 
 use embedded_graphics::{
     image::{Image, ImageRaw},
-    pixelcolor::BinaryColor,
+    mono_font::{ascii::FONT_6X10, MonoTextStyle},
+    pixelcolor::{BinaryColor, Rgb565},
     prelude::*,
     transform::Transform,
 };
@@ -13,27 +14,32 @@ use loader::disp::get_display;
 
 fn main() {
     let mut display = get_display().unwrap();
-    let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./rust.raw"), 64);
-    let start = Point::new(0, 0);
-    let mut im = Image::new(&raw, start);
+    // let raw: ImageRaw<BinaryColor> = ImageRaw::new(include_bytes!("./rust.raw"), 64);
+    let start = Point::new(5, 10);
+    // let mut im = Image::new(&raw, start);
 
-    im.draw(&mut *display).unwrap();
+    let style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+
+    // create text object
+    let text = embedded_graphics::text::Text::new("Hello, World", start, style);
+
+    text.draw(&mut *display).unwrap();
 
     display.flush().unwrap();
-    let mut delta = Point::new(1, 0);
+    // let mut delta = Point::new(1, 0);
     loop {
-        let start = Instant::now();
-        im.translate_mut(delta.clone());
+        // let start = Instant::now();
+        // im.translate_mut(delta.clone());
 
-        match (delta.x, im.bounding_box().top_left.x) {
-            (1, x) if x > 64 => delta = Point::new(-1, 0),
-            (-1, x) if x < 0 => delta = Point::new(1, 0),
-            (_, _) => {}
-        }
-        display.clear();
-        im.draw(&mut *display).unwrap();
-        display.flush().unwrap();
-        let total = Instant::now() - start;
-        println!("Time Taken: {}ms", total.as_millis());
+        // match (delta.x, im.bounding_box().top_left.x) {
+        //     (1, x) if x > 64 => delta = Point::new(-1, 0),
+        //     (-1, x) if x < 0 => delta = Point::new(1, 0),
+        //     (_, _) => {}
+        // }
+        // display.clear();
+        // im.draw(&mut *display).unwrap();
+        // display.flush().unwrap();
+        // let total = Instant::now() - start;
+        // println!("Time Taken: {}ms", total.as_millis());
     }
 }
